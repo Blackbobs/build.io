3c804d0c47c6c045bd9955fe7097c415fadd0e36
Black Bobs <ayodejiayodele350@gmail.com>
2024-05-29 17:54:58 +0100

    setup the codebase

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        brand: "#845EC2",
+        primary: "#D3ECF7",
+        secondary: "#1D87A4"
       },
     },
   },
