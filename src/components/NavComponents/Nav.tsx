da7ed85551fc5d8c463553e50d138cdc6b8c2785
Black Bobs <ayodejiayodele350@gmail.com>
2024-07-12 19:52:17 +0100

    ...

--- src/components/NavComponents/Nav.tsx
+++ src/components/NavComponents/Nav.tsx
@@ -4,7 +4,7 @@ import { RxAvatar } from "react-icons/rx";
 const Nav: React.FC = () => {
   return (
     <nav className='nav shadow-md'>
-        <h1 className='logo_gradient text-2xl font-bold'>build.io</h1>
+        <h1 className='logo_gradient text-2xl font-semibold'>build.io</h1>
         <div className='flex items-center gap-3'>
         {/* <button className='bg-brand text-white capitalize p-2 rounded font-medium text-xl'>donate</button> */}
         {/* user profile */}
