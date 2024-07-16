186025d7d1d667367846798a7bdb618ae75d692b
Black Bobs <ayodejiayodele350@gmail.com>
2024-07-16 09:44:39 +0100

    fix:authorization issues

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -33,20 +33,25 @@ const AuthProvider = ({ children }: PropsWithChildren) => {
   }, []); 
 
   useEffect(() => {
+    let route = window.location.pathname;
     if (token !== null) {
 
-      let route = window.location.pathname;
 
       if (token && route === "/login") {
         router.replace("/");
-      } else if (!token && route === "/") {
-        router.replace("/login");
-        errorToast("You're not Authorized");
-      } else if (token && (route === "/forgot-password" || route === "/update-password" || route === "/signup")) {
+      } 
+      // else if (!token && route === "/") {
+      //   router.replace("/login");
+      //   errorToast("You're not Authorized");
+      // }
+       else if (token && (route === "/forgot-password" || route === "/update-password" || route === "/signup")) {
         router.replace("/");
       } else {
         setShow(true);
       }
+    }else if(!token && route === "/"){
+      router.replace("/login");
+      errorToast("You're not Authorized");
     }
   }, [token]); 
 
